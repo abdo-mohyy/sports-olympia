6942d4c2b894bbabaa0dbb614e55b40339ebd9d6
Abdelrahman Mohy <a.mohiyyy@gmail.com>
2025-04-22 04:37:52 +0200

    clean the code

--- src/Website/Auth/AuthForm.jsx
+++ src/Website/Auth/AuthForm.jsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer";
+import Footer from "../../components/Footer/Footer";
 import Logo from "../../components/Logo/Logo";
 import "./auth.css";
 
@@ -71,64 +71,6 @@ export default function AuthForm() {
                 : "Create A New Account ?"}
             </Link>
           </Form>
-          {/* <Form
-            dir="ltr"
-            className="custom-form bg-light rounded p-3 m-2 outline-none shadow-sm"
-            onSubmit={(e) => e.preventDefault()}
-          >
-            <div className="d-flex justify-content-center flex-column">
-              <Logo />
-              <h2 className="text-primary text-center p-2 pb-3 mb-3 bg-primary text-light rounded">
-                {isRegister ? "Register !" : "Login !"}
-              </h2>
-            </div>
-
-            {isRegister && (
-              <Form.Group className="mb-3" controlId="name">
-                <Form.Control
-                  className="w-100 p-2 p-md-3 rounded bg-info outline-none"
-                  type="text"
-                  placeholder="Name"
-                  minLength="2"
-                  required
-                />
-              </Form.Group>
-            )}
-
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Control
-                className="w-100 p-2 p-md-3 rounded  bg-info"
-                type="email"
-                placeholder="someone@example.com"
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Control
-                type="password"
-                placeholder="password must be more than 8 char"
-                className="w-100 p-2 p-md-3 rounded bg-info"
-                minLength="8"
-                required
-              />
-            </Form.Group>
-
-            <Button
-              className="submit bg-info fw-bold text-primary hover-text-light hover-bg-primary w-100 p-1 p-md-2"
-              size="md"
-              type="submit"
-            >
-              {isRegister ? "Register !" : "Login !"}
-            </Button>
-            <Link
-              to={isRegister ? "/login" : "/register"}
-              className="text-primary fw-bold mt-2 p-1 d-inline-block hover-text-primary"
-            >
-              {isRegister
-                ? "Already Have An Account!"
-                : "Create A New Account?"}
-            </Link>
-          </Form> */}
         </div>
       </div>
       <Footer />
